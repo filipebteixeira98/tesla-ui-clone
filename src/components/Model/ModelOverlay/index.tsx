bcda9bc651b44daabc4a0a53d77899a6d66858a3
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2021-05-22 18:04:21 -0300

    feat: create useWrapperScroll hook

--- src/components/Model/ModelOverlay/index.tsx
+++ src/components/Model/ModelOverlay/index.tsx
@@ -1,9 +1,64 @@
-import React from 'react';
+import React, { useState, useCallback, useLayoutEffect } from 'react';
+import { useTransform } from 'framer-motion';
+
+import { CarModel } from '../ModelsContext';
+
+import useWrapperScroll from '../useWrapperScroll';
 
 import { Container } from './styles';
 
-const ModelOverlay: React.FC = ({ children }) => {
-  return <Container>{children}</Container>;
+interface Props {
+  model: CarModel;
+}
+
+type SectionDimensions = Pick<HTMLDivElement, 'offsetTop' | 'offsetHeight'>;
+
+const ModelOverlay: React.FC<Props> = ({ children, model }) => {
+  const getSectionDimensions = useCallback(() => {
+    return {
+      offsetTop: model.sectionRef.current?.offsetTop,
+      offsetHeight: model.sectionRef.current?.offsetHeight,
+    } as SectionDimensions;
+  }, [model.sectionRef]);
+
+  const [dimensions, setDimensions] = useState<SectionDimensions>(
+    getSectionDimensions()
+  );
+
+  useLayoutEffect(() => {
+    function onResize() {
+      window.requestAnimationFrame(() => setDimensions(getSectionDimensions()));
+    }
+
+    window.addEventListener('resize', onResize);
+
+    return () => window.removeEventListener('resize', onResize);
+  }, [setDimensions, getSectionDimensions]);
+
+  const { scrollY } = useWrapperScroll();
+
+  const sectionScrollProgress = useTransform(
+    scrollY,
+    (y) => (y - dimensions.offsetTop) / dimensions.offsetHeight
+  );
+
+  // React.useEffect(() => {
+  //   sectionScrollProgress.onChange((value) =>
+  //     console.log({ sectionScrollProgress: value })
+  //   );
+  // }, [sectionScrollProgress]);
+
+  const opacity = useTransform(
+    sectionScrollProgress,
+    [-0.42, -0.05, 0.05, 0.42],
+    [0, 1, 1, 0]
+  );
+
+  const pointerEvents = useTransform(opacity, (value) =>
+    value > 0 ? 'auto' : 'none'
+  );
+
+  return <Container style={{ opacity, pointerEvents }}>{children}</Container>;
 };
 
 export default ModelOverlay;
