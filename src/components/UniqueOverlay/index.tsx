e0bb1f13d032c3af32deab2b99e76603de7c5136
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2021-05-22 18:47:39 -0300

    feat: create unique overlay component

---
+++ src/components/UniqueOverlay/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useTransform } from 'framer-motion';
+
+import useWrapperScroll from '../Model/useWrapperScroll';
+
+import { Container, Header, Logo, Burger, Footer } from './styles';
+
+const UniqueOverlay: React.FC = () => {
+  const { scrollYProgress } = useWrapperScroll();
+
+  const opacity = useTransform(scrollYProgress, [0.9, 1], [0, 1]);
+
+  return (
+    <Container>
+      <Header>
+        <Logo />
+        <Burger />
+      </Header>
+      <Footer style={{ opacity }}>
+        <ul>
+          <li>
+            <a href="#made">UI Clone</a>
+          </li>
+          <li>
+            <a href="#with">made with 💜</a>
+          </li>
+          <li>
+            <a href="#love">by Filipe Teixeira</a>
+          </li>
+        </ul>
+      </Footer>
+    </Container>
+  );
+};
+
+export default UniqueOverlay;
